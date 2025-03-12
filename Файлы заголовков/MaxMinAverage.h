c0f01b89608c7fc9e481e7eb0847afcd39919520
Green <osinaleks4@tutamail.com>
2025-03-12 20:43:03 +0300

    Добавлены файлы решения 2 заданий

---
+++ Файлы заголовков/MaxMinAverage.h
@@ -0,0 +1,9 @@
+#ifndef MAXMINAVERAGE_H
+#define MAXMINAVERAGE_H
+
+#include <iostream>
+int64_t MaxElement(const int64_t arr[], int32_t size);
+int64_t MinElement(const int64_t arr[], int32_t size);
+double CalculateAverage(const int64_t arr[], int32_t size);
+
+#endif
