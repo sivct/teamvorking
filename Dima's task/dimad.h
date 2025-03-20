0ac03690c5b633c7076b6a424c9e61534dcecd58
DZMITRY <kotron.pro@mail.ru>
2025-03-21 00:11:27 +0300

    Добавлено задание Димы

---
+++ Dima's task/dimad.h
@@ -0,0 +1,13 @@
+#ifndef DIMA_H
+#define DIMA_H
+
+#include <iostream>
+
+void reverseArray(int32_t* arr, int32_t size);
+
+bool isPrime(int32_t num);
+
+int32_t sumOfPrimes(int32_t* arr, int32_t size);
+
+
+#endif
